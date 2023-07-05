ab5ef43d5400e9ffb8daeb4e014a3b892d5a56bc
MelnikovEI <64632765+MelnikovEI@users.noreply.github.com>
2023-07-05 11:56:20 +0300

    Add data validation for register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,7 +1,9 @@
-import json
+import django.http
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.templatetags.static import static
 from phonenumber_field.phonenumber import PhoneNumber
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -60,28 +62,66 @@ def product_list_api(request):
     })
 
 
+def is_positive_integer(val):
+    if not isinstance(val, int):
+        return False
+    elif val < 1:
+        return False
+    return True
+
+
 @api_view(['POST'])
 def register_order(request):
     try:
-        order_data = request.data # json.loads(request.body.decode())
+        order_data = request.data  # json.loads(request.body.decode())
     except ValueError:
-        return JsonResponse({
-            'error': 'No data returned in order',
-        })
+        return Response({'error': 'No data returned in order'}, status=status.HTTP_417_EXPECTATION_FAILED)
+    try:
+        phone_number = order_data['phonenumber']
+    except KeyError:
+        return Response({'error': 'No phone number returned in order'}, status=status.HTTP_417_EXPECTATION_FAILED)
+    try:
+        order_elements = order_data['products']
+    except KeyError:
+        return Response({'error': 'No products list returned in order'}, status=status.HTTP_417_EXPECTATION_FAILED)
+    if not isinstance(order_elements, list):
+        return Response(
+            {'error': f"'Products' should contain a list, not a {type(order_elements).__name__}"},
+            status=status.HTTP_417_EXPECTATION_FAILED
+        )
+    if not order_elements:
+        return Response({'error': f"'Products' can\'t be empty"}, status=status.HTTP_417_EXPECTATION_FAILED)
 
     order = Order.objects.create(
-        first_name=order_data['firstname'],
-        last_name=order_data['lastname'],
-        phone_number=PhoneNumber.from_string(phone_number=order_data['phonenumber'], region='RU').as_e164, #order_data['phonenumber'],
-        address=order_data['address'],
+        first_name=order_data.get('firstname', ''),
+        last_name=order_data.get('lastname', ''),
+        phone_number=PhoneNumber.from_string(phone_number=phone_number, region='RU').as_e164,
+        address=order_data.get('address', ''),
     )
-    order_elements = order_data['products']
     for element in order_elements:
+        product_id = element['product']
+        if not is_positive_integer(product_id):
+            return Response(
+                {'error': f"Product_id '{product_id}' is not positive integer"},
+                status=status.HTTP_417_EXPECTATION_FAILED
+            )
+        try:
+            product = get_object_or_404(Product, pk=product_id)
+        except django.http.Http404:
+            return Response(
+                {'error': f"Product_id '{product_id}' doesn\'t exist in database"},
+                tatus=status.HTTP_404_NOT_FOUND
+            )
+
+        quantity = element['quantity']
+        if not is_positive_integer(quantity):
+            return Response(
+                {'error': f"Quantity '{quantity}' is not positive integer"},
+                status=status.HTTP_417_EXPECTATION_FAILED,
+            )
         OrderElement.objects.create(
-            product=Product.objects.get(pk=element['product']),
-            quantity=element['quantity'],
+            product=product,
+            quantity=quantity,
             order=order,
         )
-    print(order_data)
-    # return JsonResponse({})
-    return Response({})
+    return Response({}, status=status.HTTP_201_CREATED)
