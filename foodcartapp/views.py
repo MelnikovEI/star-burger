518347c4d42efcf02d9c832eda5670dacac19e8c
MelnikovEI <64632765+MelnikovEI@users.noreply.github.com>
2023-08-03 21:14:04 +0300

    Simplify code

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -65,7 +65,6 @@ def product_list_api(request):
 @api_view(['POST'])
 def register_order(request):
     serializer = OrderSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
